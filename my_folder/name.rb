ff25daddcac9e4227e28f2905a99082701842e6d
Jay Khoury <kyttcar@gmail.com>
2019-02-22 21:35:19 -0500

    Updating name.rb again

--- my_folder/name.rb
+++ my_folder/name.rb
@@ -1,9 +1,12 @@
-puts " Dear user please enter your name here:"
+puts " Dear user please enter your first name here:"
 user_name = gets.chomp
 
-puts " Welcome to this beginning program Dear #{user_name}."
+puts "Please enter your last name here:"
+last_name = gets.chomp
+
+puts " Welcome to this beginning program Dear #{user_name} #{last_name}."
 
 
 10.times do |i|
-  puts user_name
+  puts user_name +' ' + last_name
 end
