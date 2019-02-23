e4ac92bebd7fc305adf164bf018eca2c07d878be
Jay Khoury <kyttcar@gmail.com>
2019-02-22 21:03:11 -0500

    Adding Two Exercises of Variable chapter

---
+++ my_folder/age.rb
@@ -0,0 +1,13 @@
+puts "Dear User please enter your age here: "
+age = gets.chomp
+age = age.to_i
+
+
+puts "In 10 Years, you will be :"
+puts age + 10
+puts "In 20 Years, you will be :"
+puts age + 20
+puts "In 30 Years, you will be :"
+puts age + 30
+puts "In 40 Years, you will be :"
+puts age + 40
---
+++ my_folder/name.rb
@@ -0,0 +1,4 @@
+puts " Dear user please enter your name here:"
+user_name = gets.chomp
+
+puts " Welcome to this beginning program Dear #{user_name}."
